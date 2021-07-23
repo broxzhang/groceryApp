2afe932763163b54d271db5f38650082b3f7fef0
broxzhang <broxzhang@gmail.com>
2021-07-22 21:09:17 -0400

    add filter to the seafood page

--- js/product.js
+++ js/product.js
@@ -4,8 +4,85 @@ const fakeProduct = {
     aisles: 'seafood',
     price: 10.99,
     category: 'fish',
-    isFresh: true,
+    type: 'fresh',
     avg: 7.93,
     photo: 'img/salmon_fillet.jpeg'
 }
+
+const seafood = [
+    {    
+        id: 1,
+        productName: 'Fresh Canadian Atlantic Salmon Fillet',
+        aisles: 'seafood',
+        price: 10.99,
+        category: 'fish',
+        type: 'fresh',
+        avg: 7.93,
+        photo: 'img/salmon_fillet.jpeg',
+        ingredients: ''
+    },
+    {    
+        id: 2,
+        productName: 'Crab flavoured pollock flakes',
+        aisles: 'seafood',
+        price: 8.99,
+        category: 'fish',
+        type: 'fresh',
+        avg: null,
+        photo: 'img/crab-flavored.jpeg'
+    },
+    {    
+        id: 3, 
+        productName: 'Fresh Atlantic Salmon Steak',
+        aisles: 'seafood',
+        price: 15.99,
+        category: 'fish',
+        type: 'fresh',
+        avg: 8.81,
+        photo: 'img/fresh_atlantic.jpeg'
+    },
+    {
+        id: 4,
+        productName: 'Pollock and Shrimp Salad',
+        aisles: 'seafood',
+        price: 8.99,
+        category: 'seafood',
+        type: 'prepared',
+        avg: 4.96,
+        photo: 'img/pollock-and-srimp-salad.jpeg'
+    },
+    {
+        id: 5,
+        productName: 'Salmon Fillet with Rosée Sauce',
+        aisles: 'seafood',
+        price: 12.43,
+        category: 'fish',
+        type: 'prepared',
+        avg: 9.73,
+        photo: 'img/Salmon-Fillet-Rosée-Sauce.jpeg'
+    },
+    {
+        id: 6,
+        productName: 'Frozen sliced smoked coho salmon',
+        aisles: 'seafood',
+        price: 21.99,
+        category: 'fish',
+        type: 'frozen',
+        avg: 21.99,
+        photo: 'img/Frozen-sliced-smoked-coho-salmon.jpeg',
+        ingredients: 'We strive to always offer you the most up-to-date product information. However, manufacturers may change this information at any time. We recommend that you read labels and packaging before consuming any product to ensure that you have the updated information regarding the ingredients, the nutritional values, as well as any allergy warnings.'
+    },
+    {
+        id: 7,
+        productName: 'Frozen Uncooked Pacific White Shrimp',
+        aisles: 'seafood',
+        price: 10.99,
+        category: 'fish',
+        type: 'fresh',
+        avg: 7.93,
+        photo: 'img/Frozen-Uncooked-Pacific-White-Shrimp.jpeg'
+    }
+];
+
+export default seafood;
 
--- js/seafood.js
+++ js/seafood.js
@@ -1,39 +1,47 @@
-const seafood = [
-    {    
-        id: 1,
-        productName: 'Fresh Canadian Atlantic Salmon Fillet',
-        aisles: 'seafood',
-        price: 10.99,
-        category: 'fish',
-        isFresh: true,
-        avg: 7.93,
-        photo: 'img/salmon_fillet.jpeg'
-    },
-    {    
-        id: 2,
-        productName: 'Crab flavoured pollock flakes',
-        aisles: 'seafood',
-        price: 8.99,
-        category: 'fish',
-        isFresh: true,
-        avg: null,
-        photo: 'img/crab-flavored.jpeg'
-    },
-    {    
-        id: 3, 
-        productName: 'Fresh Atlantic Salmon Steak',
-        aisles: 'seafood',
-        price: 15.99,
-        category: 'fish',
-        isFresh: true,
-        avg: 8.81,
-        photo: 'img/fresh_atlantic.jpeg'
-    }
-]
+// const seafood = [
+//     {    
+//         id: 1,
+//         productName: 'Fresh Canadian Atlantic Salmon Fillet',
+//         aisles: 'seafood',
+//         price: 10.99,
+//         category: 'fish',
+//         isFresh: true,
+//         avg: 7.93,
+//         photo: 'img/salmon_fillet.jpeg'
+//     },
+//     {    
+//         id: 2,
+//         productName: 'Crab flavoured pollock flakes',
+//         aisles: 'seafood',
+//         price: 8.99,
+//         category: 'fish',
+//         isFresh: true,
+//         avg: null,
+//         photo: 'img/crab-flavored.jpeg'
+//     },
+//     {    
+//         id: 3, 
+//         productName: 'Fresh Atlantic Salmon Steak',
+//         aisles: 'seafood',
+//         price: 15.99,
+//         category: 'fish',
+//         isFresh: true,
+//         avg: 8.81,
+//         photo: 'img/fresh_atlantic.jpeg'
+//     }
+// ]
+
+import seafood from './product.js';
+// import seafood from './product.js';
+
 
 const seafoodSection = document.querySelector(".products-container");
 
 window.addEventListener("DOMContentLoaded", function(){
+    displayFood(seafood)
+})
+
+function displayFood(seafood) {
     let displayFood = seafood.map( (item) => {
         const pizKg = calculateKg(item.price);
 
@@ -46,12 +54,64 @@ window.addEventListener("DOMContentLoaded", function(){
         <span>(200 g avg.)</span>
         <span>${pizKg}/kg ${item.price}/lb</span>
         <!-- <img style="width: 40px; height: 40px; color: black;" src="shopping_cart.png" /> -->
+        <i class="fas fa-shopping-cart"></i>
     </div>`
     })
     displayFood = displayFood.join("");
     seafoodSection.innerHTML = displayFood;
-})
+}
 
-calculateKg = (price) => {
+function calculateKg(price) {
     return (price * 2.205).toFixed(2);
 }
+
+document.querySelectorAll(".filter").forEach(item => {
+    let filteredSeafood = []
+    item.addEventListener('click', ()=>{
+        console.log(item.innerHTML)
+        switch(item.innerHTML){
+            case 'Fresh Fish':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='fresh') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+            case 'Fresh Seafood':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='fresh') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+            case 'Frozen Fish':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='frozen') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+            case 'Frozen Seafood':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='frozen') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+            case 'Prepared Fish':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='prepared') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+            case 'Prepared Seafood':
+                console.log('here')  
+                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='prepared') 
+                console.log(filteredSeafood)
+                displayFood(filteredSeafood);
+                break;
+                                
+        }
+    })
+})
+
+
+// filterAnchor.addEventListener('click', function(){
+//     console.log('here')
+// })
