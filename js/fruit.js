f52d13dc3bb788c1215d2657fa814da48a0882b0
caizhangbin <71842756+caizhangbin@users.noreply.github.com>
2021-08-03 15:04:07 -0400

    edite fruit p3

--- js/fruit.js
+++ js/fruit.js
@@ -9,45 +9,46 @@ function myFunction2() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-// When the user clicks on div, open the popup
-// function myFunction2() {
-//     var popup = document.getElementById("myPopup");
-//     popup.classList.toggle("show");
-// }
 
-// function calculateAmount(val) {
-//     var tot_price = val * 1.23;
-//     /*display the result*/
-//     var divobj = document.getElementById('tot_amount');
-//     divobj.value = tot_price;
-// }
+//calculate the total price
+function mult(value) {
+    var x;
+    x = 1.23 * value
+    x=parseFloat(x).toFixed(2);
+    document.getElementById('totalPrice').value = x;
+}
 
-
-// $(document).ready(function(){
-// 	$(".checkout").on("keyup", ".quantity", function(){
-// 		var price = +$(".price").data("price");
-// 		var quantity = +$(this).val();
-// 		$("#total").text("$" + price * quantity);
-// 	})
-// })
-
-function mult (value){
-    var x;
-    x= 1.23*value
-    document.getElementById('totalPrice').value=x;
-}
-function mult2 (value){
-    var x;
-    x= 0.49*value
-    document.getElementById('totalPrice2').value=x;
-}
-function mult3 (value){
-    var x;
-    x= 0.35*value
-    document.getElementById('totalPrice3').value=x;
-}
-function mult4 (value){
-    var x;
-    x= 0.40*value
-    document.getElementById('totalPrice4').value=x;
-}
+// cockies store the number after refreshing
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
