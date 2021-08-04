fd0e0f82f6a762dae8900158c484919f7ce665de
caizhangbin <71842756+caizhangbin@users.noreply.github.com>
2021-08-03 23:20:54 -0400

    upload new pics

--- js/fruit.js
+++ js/fruit.js
@@ -19,36 +19,12 @@ function mult(value) {
 }
 
 // cockies store the number after refreshing
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-  function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+function WriteCookie() {
+    if( document.myform.customer.value == "" ) {
+       alert("Enter some value!");
+       return;
     }
-    return "";
-  }
-  
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
+    cookievalue = escape(document.myform.customer.value) + ";";
+    document.cookie = "name=" + cookievalue;
+    document.write ("Setting Cookies : " + "name=" + cookievalue );
+ }
