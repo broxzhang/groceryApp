6220251008ad780e3139609f52df3f2d0017a90e
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-17 17:54:12 -0400

    Create addProduct.php

---
+++ php/addProduct.php
@@ -0,0 +1,10 @@
+<?php
+
+      $name = $_POST["name"];
+      $price = $_POST["price"];
+      $aisle = $_POST["Aisle"];
+      $size = $_POST["Size"];
+      $image = $_POST["image"];
+      $description = $_POST["description"];
+
+?>
