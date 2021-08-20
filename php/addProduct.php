42f07fb2552da1668c5bdd854a23e3f5069d3950
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-20 18:47:12 -0400

    Update addProduct.php

--- php/addProduct.php
+++ php/addProduct.php
@@ -1,48 +1,56 @@
 <?php
 
+function appendEl($el, $parent){
+  global $xml;
+  $node=$xml->createElement($el);
+  $parent->appendChild($node);
+  return $node;
+}
+function appendVal($value, $parent)
+{
+  global $xml;
+  $value=$xml->createTextNode($value);
+  $parent->appendChild($value);
+  return $value;
+}
+
       $name = $_POST["name"];
       $p = $_POST["price"];
       $aisle = $_POST["Aisle"];
       $size = $_POST["Size"];
       $img = $_POST["image"];
       $desc = $_POST["description"];
+      $categ =$_POST["category"];
+      $ave = $_POST["average"];
+      $typ = $_POST["type"];
 
       $xml=new DomDocument('1.0');
       $xml->load("products.xml");
-      $root=$xml->$xml->getElementByTagName("products")->item(0);
-      $product=$xml->createElement("product");
-      $product->setAttribute("id", 18);
-      switch($aisle){
-        case "fruits":
-          $fruits->appendChild($product);
-          break;
-        case "vegetables":
-          $vegetables->appendChild($product);
-          break;
-        case "beverages":
-          $beverages->appendChild($product);
-          break;
-        case "dairy":
-          $dairy->appendChild($product);
-          break;
-        case "seafood":
-          $seafood->appendChild($product);
-          break;
-        case "snacks":
-          $snacks->appendChild($product);
-          break;
-      }
-      $productName=$xml->createElement("productName", $name);
-      $product->appendChild($productName);
-      $price=$xml->createElement("price", $p);
-      $product->appendChild($price);
-      $aisles=$xml->createElement("aisles", $aisle);
-      $product->appendChild($aisles);
-      $image=$xml->createElement("image", $img);
-      $product->appendChild($image);
-      $description=$xml->createElement("description", $desc);
-      $product->appendChild($description);
+      $root=$xml->getElementByTagName("products")->item(0);
+      $product=appendEl("product", $root);
 
+
+      $product->setAttribute("id", 18);
+      
+      $productName=appendEl("productName", $product);
+      appendVal("$name", $productName);
+      $price=appendEl("price", $product);
+      appendVal("$p", $price);
+      $aisles=appendEl("aisles", $product);
+      appendVal("$aisles", $aisle);
+      $image=appendEl("photo", $product);
+      appendVal("$img", $image);
+      $description=appendEl("description", $product);
+      appendVal("$desc", $description);
+      $category=appendEl("category", $product);
+      appendVal("$categ", $category);
+      $type=appendEl("type", $product);
+      appendVal("$typ", $type);
+      $average=appendEl("avg", $product);
+      appendVal("$ave", $average);
+
+     
+      $xml->formatOutput=true;
       $xml->save("products.xml");
       
 
