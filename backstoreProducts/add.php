306b11f7675aba41379404332f6cfe52ce27fc12
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-25 15:33:49 -0400

    Update add.php

--- backstoreProducts/add.php
+++ backstoreProducts/add.php
@@ -3,7 +3,8 @@
 	if(isset($_POST['add'])){
 		//open xml file
 		$productslist = simplexml_load_file('../database/products.xml');
-		$product = $productslist->addChild('product');
+		$products = $productslist->products;
+		$product = $products->addChild('product');
 		$product->addChild('id', $_POST['id']);
 		$product->addChild('productName', $_POST['productName']);
 		$product->addChild('aisles', $_POST['aisles']);
