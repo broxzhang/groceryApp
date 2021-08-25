f2a869fa5c3fcce1d88fe79b88260604cf9c4207
broxzhang <broxzhang@gmail.com>
2021-08-25 19:48:08 -0400

    add modify qty in shopping cart page

--- php/addToCart.php
+++ php/addToCart.php
@@ -15,13 +15,39 @@ array_push($cart, $newItem);
 
 $combinedCart = array();
 
-$_SESSION["Cart"] = $cart;
+
+$ids = array();
+
+$NewCart = array();
+
 
 $numOfItem = 0;
 
 foreach ($cart as $item) {
     $numOfItem +=  intval($item->numberOfItem);
 }
+
+foreach ($cart as $item) {
+    if (array_key_exists($item->itemId, $ids)) {
+        $ids[$item->itemId] = (intval($ids[$item->itemId]) + intval($item->numberOfItem));
+    } else {
+        $ids[$item->itemId] = intval($item->numberOfItem);
+    }
+}
+
+
+foreach ($ids as $key => $value) {
+    if ($key != "" and $key != "?10") {
+        array_push($NewCart,  (object)([
+            'itemId' => $key,
+            'numberOfItem' => $value
+        ]));
+    }
+}
+print_r($NewCart);
+
+$_SESSION["Cart"] = $NewCart;
+
 
 echo $numOfItem;
 
