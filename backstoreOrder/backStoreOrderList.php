c87e6efd55e965ebdf359999a79f650980c03ea4
caizhangbin <71842756+caizhangbin@users.noreply.github.com>
2021-08-25 13:36:54 -0400

    backstore links fixed

--- backstoreOrder/backStoreOrderList.php
+++ backstoreOrder/backStoreOrderList.php
@@ -23,8 +23,8 @@
 
     <div class="navbar">
         <a href="../index.php">Home</a>
-        <a href="backstoreProducts/backStoreProductsList.php">Product List</a>
-        <a href="backStoreUserList/backStoreUserList.php">User List</a>
+        <a href="../backstoreProducts/backStoreProductsList.php">Product List</a>
+        <a href="../backStoreUserList/backStoreUserList.php">User List</a>
         <a href="backStoreOrderList.php">Order List</a>
         <div class="subnav">
             <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
