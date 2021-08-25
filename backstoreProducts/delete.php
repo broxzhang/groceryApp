c9001832a65ca82c3c2f926347295a36da4c3c16
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-25 15:40:37 -0400

    Update delete.php

--- backstoreProducts/delete.php
+++ backstoreProducts/delete.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$id = $_GET['id'];
+	$productName = $_GET['productName'];
 
 	$productslist = simplexml_load_file('../database/products.xml');
 
@@ -8,15 +8,15 @@
 	$index = 0;
 	$i = 0;
 
-	foreach($productslist->product as $product){
-		if($product->id == $id){
+	foreach($productslist->products->product as $product){
+		if($product->productName == $productName){
 			$index = $i;
 			break;
 		}
 		$i++;
 	}
 
-	unset($productslist->product[$index]);
+	unset($productslist->products->product[$index]);
 	file_put_contents('../database/products.xml', $productslist->asXML());
 
 	$_SESSION['message'] = 'Member deleted successfully';
