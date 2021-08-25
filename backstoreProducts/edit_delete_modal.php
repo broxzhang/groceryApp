c92d1e94d7fab20afc0b164def76d7c32dd5852e
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-25 15:29:00 -0400

    Update edit_delete_modal.php

--- backstoreProducts/edit_delete_modal.php
+++ backstoreProducts/edit_delete_modal.php
@@ -86,7 +86,7 @@
 </div>
 
 <!-- Delete -->
-<div class="modal fade" id="delete_<?php echo $row->id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="delete_<?php echo $row->productName; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -95,11 +95,11 @@
             </div>
             <div class="modal-body">	
             	<p class="text-center">Are you sure you want to Delete</p>
-				<h2 class="text-center"><?php echo $row->id.' '.$row->productName; ?></h2>
+				<h2 class="text-center"><?php echo $row->productName; ?></h2>
 			</div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-                <a href="delete.php?id=<?php echo $row->id; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Yes</a>
+                <a href="delete.php?id=<?php echo $row->productName; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Yes</a>
             </div>
 
         </div>
