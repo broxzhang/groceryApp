b07e10f9e949589d84d624885360386cbf6b2d19
mikibi <87385214+mikibi@users.noreply.github.com>
2021-08-24 21:48:55 -0400

    Update delete.php

--- backstoreProducts/delete.php
+++ backstoreProducts/delete.php
@@ -2,7 +2,7 @@
 	session_start();
 	$id = $_GET['id'];
 
-	$productslist = simplexml_load_file('database/products.xml');
+	$productslist = simplexml_load_file('../database/products.xml');
 
 	//we're are going to create iterator to assign to each user
 	$index = 0;
@@ -17,7 +17,7 @@
 	}
 
 	unset($productslist->product[$index]);
-	file_put_contents('database/products.xml', $productslist->asXML());
+	file_put_contents('../database/products.xml', $productslist->asXML());
 
 	$_SESSION['message'] = 'Member deleted successfully';
 	header('location: backStoreProductsList.php');
