29879a3e3531f25998d2ac7a240e26c2996f069c
caizhangbin <71842756+caizhangbin@users.noreply.github.com>
2021-08-31 09:55:31 -0400

    Authentication

--- backstoreMain.php
+++ backstoreMain.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
  //set the session on the login page
- $_SESSION['loggedIn'] = true;  
-
+ 
  //on the second page you check if that session is true, else redirect to the login page  
-  if($_SESSION['loggedIn']){}
+  if($_SESSION['loggedIn']){
+    $_SESSION['loggedIn'] = true;  
+
+  }
       //allow
   else{
       //redirect to the login page
